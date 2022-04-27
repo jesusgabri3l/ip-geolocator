18d93a3590c32de1e8a2c2b104bd5eb1bba3eccb
Jesús Gabriel <jesus15202009@gmail.com>
2022-04-26 21:26:59 -0500

    build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://jesusgabri3l.github.io/ip-geolocator/",
   plugins: [react()]
 })
